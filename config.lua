54e9d75740cb6f178e9ed385e4cc4342d990c14e
K1Dev <devgodno1@gmail.com>
2022-10-27 02:22:23 +0700

    1-K1Dev-Inventory2.0
    1-K1Dev-Inventory2.0

--- config.lua
+++ config.lua
@@ -6,12 +6,23 @@ Config.Usingrdx11 = false  --[[ No modifications are required. ]]
 Config.SoundEffects = true --[[ Do you turn on the sound system? ]]
 
 -- Main
-Config.OpenKey = 'I' --[[ Inventory OpenKey ]] 
-Config.ExtraCloseKey = 75 --[[ key to close Js  (https://keycode.info/) ]]
-Config.CloseItems = {'ammo'} --[[ Use the item and it will close the bag itself. ]]
+Config.OpenKey = 'I'
+Config.ExtraCloseKey = 75 --[[ button to close Js  (https://keycode.info/) ]]
+Config.CloseItems = {'ufoitem'} --[[ Use the item and it will close the bag itself. ]]
 Config.Cash = true  --[[ show money ]]
 Config.DirtyCash = true --[[ show DirtyCash ]]
 Config.Hotbar = true --[[ Open the slot to put things in? ]]
+
+
+Config['labelnormal']  = 'ตู้เชฟปกติ'
+Config['labelupgrade']  =  'ตู้เชฟอัพเกรด'
+
+--[[  Items to increase capacity ]]
+Config['Weightupgradeitems']  = '9TAILS'
+
+Config['Upgradeweight'] =  500
+Config['Normalweight'] = 100
+
 
 
 --[[
